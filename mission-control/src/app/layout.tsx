a5c68023e5f3ad5e3089999a26b317d7a3e73133
Billy Nelms <campoffice@Camps-MacBook-Pro-88.local>
2026-02-27 15:02:44 -0600

    Add otter favicon to Mission Control
    - SVG emoji favicon (🦦) for easy browser tab identification
    - Added to metadata icons in layout.tsx

--- mission-control/src/app/layout.tsx
+++ mission-control/src/app/layout.tsx
@@ -4,6 +4,15 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'B.O.B. Mission Control',
   description: 'Bot On Board - First Baptist Church Arlington',
+  icons: {
+    icon: [
+      {
+        url: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🦦</text></svg>',
+        type: 'image/svg+xml',
+      }
+    ],
+    apple: 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">🦦</text></svg>',
+  },
 }
 
 export default function RootLayout({
